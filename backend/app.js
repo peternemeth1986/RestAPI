cfbb7cf0a8fc8b30d382142fe72c60bcb4af59e0
Peter Nemeth <peter.nemeth1986@gmail.com>
2024-06-23 12:05:52 +0200

    427. Handling errors

--- backend/app.js
+++ backend/app.js
@@ -52,15 +52,15 @@ app.use('/graphql', graphqlHTTP({
     schema: graphqlSchema,
     rootValue: graphqlResolver,
     graphiql: true,
-    // customFormatErrorFn(err) {
-    //     if (!err.originalError) {
-    //         return err;
-    //     }
-    //     const data = err.originalError.data;
-    //     const message = err.message || 'An error occurred.';
-    //     const code = err.originalError.code || 500;
-    //     return { message: message, status: code, data: data };
-    // }
+    customFormatErrorFn(err) {
+        if (!err.originalError) {
+            return err;
+        }
+        const data = err.originalError.data;
+        const message = err.message || 'An error occurred.';
+        const code = err.originalError.code || 500;
+        return { message: message, status: code, data: data };
+    }
 }));
 
 app.use((error, req, res, next) => {
