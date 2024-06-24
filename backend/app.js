7d1172ca3efff29c6aec5848b1d95361e91c289d
Peter Nemeth <peter.nemeth1986@gmail.com>
2024-06-24 20:55:55 +0200

    428. Connection the Frontend to the Graphql API

--- backend/app.js
+++ backend/app.js
@@ -45,6 +45,9 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*'); // '*' allows every client access to the resource
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
     next();
 })
 
